fc1e95b22a0915953ba0c59ce750d082a5919211
simon@alle-schindlers.de <simon@alle-schindlers.de>
2023-08-07 01:03:19 +0200

    add guard to script

--- README.md
+++ README.md
@@ -29,5 +29,5 @@ the ports and the storage path have to be the same as in the env-file.
 then run the container
 
 ```bash
-docker run -d --name essensplaner -v essensplaner_storage:/usr/essensplaner/storage/ -p 80:80 --env-file ./.env iowi/essensplaner:1.0
+docker run -d --name essensplaner -v essensplaner_storage:/usr/essensplaner/storage/ -p 80:80 --env-file ./.env --restart unless-stopped iowi/essensplaner:1.0
 ```
--- quickstart.sh
+++ quickstart.sh
@@ -1,3 +1,9 @@
+if [ ! -f "./.env" ]; then
+    echo "you didn't rename default.env -> .env"
+    echo "aborting..."
+    exit
+fi
+
 export $(grep -v '^#' .env | xargs -d '\n')
 
 echo "building image: iowi/essensplaner:${ESSENSPLANER_VERSION} ..."
@@ -5,7 +11,7 @@ echo "building image: iowi/essensplaner:${ESSENSPLANER_VERSION} ..."
 docker build -t iowi/essensplaner:"${ESSENSPLANER_VERSION}" --build-arg HOST="${ESSENSPLANER_HOST}" --build-arg PORT="${ESSENSPLANER_PORT}" --build-arg API_PATH="${ESSENSPLANER_API_PATH}" .
 
 echo "starting container: iowi/essensplaner:${ESSENSPLANER_VERSION} ..."
-docker run -d --name essensplaner -v essensplaner_storage:"${ESSENSPLANER_STORAGE_PATH}" -p "${ESSENSPLANER_PORT}":"${ESSENSPLANER_PORT}" --env-file .env iowi/essensplaner:"${ESSENSPLANER_VERSION}"
+docker run -d --name essensplaner -v essensplaner_storage:"${ESSENSPLANER_STORAGE_PATH}" -p "${ESSENSPLANER_PORT}":"${ESSENSPLANER_PORT}" --env-file .env --restart unless-stopped iowi/essensplaner:"${ESSENSPLANER_VERSION}"
 
 unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
 
